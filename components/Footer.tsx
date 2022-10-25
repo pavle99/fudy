7001f98de22d5d93175d9b6e9c3d314106f62964
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-10-25 11:26:06 +0200

    add Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,25 @@
+import { UilFacebook, UilGithub, UilLinkedin } from "@iconscout/react-unicons";
+import Image from "next/image";
 import React from "react";
+import css from "../styles/Footer.module.css";
+import Logo from "../assets/Logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={css.container}>
+      <span>ALL RIGHTS RESERVED</span>
+      <div className={css.social}>
+        <UilFacebook size={45} />
+        <UilGithub size={45} />
+        <UilLinkedin size={45} />
+      </div>
+
+      <div className={css.logo}>
+        <Image src={Logo} alt="" width={70} height={70} />
+        <span>FUDY</span>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
